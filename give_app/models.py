8c4c97169a1a611bbcecf00dbd4618bac081ee7c
Kamil Orliński <kamil.orlinski@gmail.com>
2020-05-22 17:12:01 +0200

    pierwsze kroki w aplikacji webowej

--- give_app/models.py
+++ give_app/models.py
@@ -1,29 +1,41 @@
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
-TYPE_CHOOICES = (
-    ("1", "fundacja"),
-    ("2", "organizacja pozarządowa"),
-    ("3", "zbiórka lokalna"),
-)
+
 class Category(models.Model):
     name = models.CharField(max_length=16)
 
+    def __str__(self):
+        return self.name
+
+
 class Institution(models.Model):
+    FUNDACJA = 'FUN'
+    ORGANIZACJA = 'OP'
+    ZBIÓRKA = 'ZL'
+    TYPE_CHOOICES = [
+        (FUNDACJA, "fundacja"),
+        (ORGANIZACJA, "organizacja pozarządowa"),
+        (ZBIÓRKA, "zbiórka lokalna"),
+    ]
     name = models.CharField(max_length=40)
-    type = models.CharField(max_length=20, choices=TYPE_CHOOICES, default="1")
+    type = models.PositiveIntegerField(choices=TYPE_CHOOICES, default=FUNDACJA)
     categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
 
 class Donation(models.Model):
     quantity = models.IntegerField()
     categories = models.ManyToManyField(Category)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     adress = models.CharField(max_length=128)
-    phone_number = models.IntegerField(max_length=12)
+    phone_number = models.IntegerField()
     city = models.CharField(max_length=36)
-    zip_code = models.IntegerField(max_length=6)
+    zip_code = models.IntegerField()
     pick_up_date = models.DateField
     pick_up_time = models.DateTimeField
     pick_up_comment = models.TextField(max_length=300)
-    user = models.ForeignKey(AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
--- give_app/views.py
+++ give_app/views.py
@@ -1,21 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views import View
 
 
 # Create your views here.
+from give_app.forms import SignUpForm
+from give_app.models import Donation, Institution
+
+
 
 class LandingPageView(View):
-    def get(self,request):
-        return render(request,'index.html')
+    def get(self, request):
+        donation_quantity = Donation.objects.all()
+        # institution_id = Institution.objects.latest(id) 'institutions':institution_id
+        return render(request, 'index.html', {'donations':donation_quantity,})
 
 class AddDonationView(View):
-    def get(self,request):
-        return render(request,'form.html')
+    def get(self, request):
+        return render(request, 'form.html')
 
 class LoginView(View):
-    def get(self,request):
-        return render(request,'login.html')
+    def get(self, request):
+        return render(request, 'login.html')
+
 
 class RegisterView(View):
-    def get(self,request):
-        return render(request,'register.html')
+    def get(self, request):
+        return render(request, 'register.html')
+
+    # def post (self, request):
